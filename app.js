b8c95c437b2dd24c85ffb74aac1a6759c8192c0c
Josh Parinussa <yehezkieljosh@gmail.com>
2024-07-29 20:10:01 +0700

    add mysql database

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // app.js
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const errorHandler = require('./middlewares/errorHandler');
+const db = require('./models');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
 
 app.use(express.json());
 
@@ -11,5 +17,10 @@ app.use('/api/v1/users', userRoutes);
 // Use error-handling middleware
 app.use(errorHandler.notFound);
 app.use(errorHandler);
+
+// Sync database and create tables
+db.sequelize.sync().then(() => {
+    console.log('Database synchronized');
+});
 
 module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,52 @@
 // controllers/userController.js
+const bcrypt = require('bcrypt');
 const createError = require('http-errors');
-const { getUsersFromDB, getUserFromDB, createUserInDB } = require('../models/userModel');
-
-exports.getAllUsers = async (req, res, next) => {
-    try {
-        const users = await getUsersFromDB();
-        res.json(users);
-    } catch (err) {
-        next(createError(500, 'Failed to fetch users'));
-    }
-};
-
-exports.getUserById = async (req, res, next) => {
-    try {
-        const userId = req.params.id;
-        const user = await getUserFromDB(userId);
-        if (!user) {
-            return next(createError(404, 'User not found'));
-        }
-        res.json(user);
-    } catch (err) {
-        next(createError(500, 'Failed to fetch user'));
-    }
-};
+const db = require('../models');
+const { default: AsyncQueue } = require('sequelize/lib/dialects/mssql/async-queue');
+const User = db.user;
 
 exports.createUser = async (req, res, next) => {
     try {
-        const newUser = req.body;
-        const createdUser = await createUserInDB(newUser);
-        res.status(201).json(createdUser);
+        const { name, email, password } = req.body;
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create the user
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        res.status(201).json(newUser);
     } catch (err) {
-        next(createError(500, 'Failed to create user'));
+        if (err.name === 'SequelizeUniqueConstraintError') {
+            next(createError(400, 'Email already in use'));
+        } else {
+            next(createError(500, 'Failed to create user'));
+        }
     }
 };
+
+exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.findAll();
+        res.json(users)
+    } catch (err) {
+        next(createError(500, 'Failed to retrieve users'));
+    }
+}
+
+exports.getUserById = async (req, res, next) => {
+    try {
+        const userID = req.params.id;
+        const user = await User.findByPk(userID);
+        if (!user){
+            return next(createError(404, 'User not found'));
+        }
+        res.json(user)
+    } catch (err) {
+        next(createError(500, 'Failed to retrieve user'));
+    }
+}
